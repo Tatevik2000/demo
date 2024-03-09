6a6cd2d14ddca7b0541c5ac53794ac4de0c0328a
Tatev Hunanyan <124250439+Tatevik2000@users.noreply.github.com>
2024-03-09 16:16:12 +0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+/*
 resource "random_id" "RANDOM_ID" {
   byte_length = "2"
 }
@@ -294,3 +295,4 @@ module "dynamodb_table" {
   source = "./Modules/Dynamodb"
   name   = "assets-table-${var.environment_name}"
 }
+*/
